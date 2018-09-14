369f401d723bb5b51c546e024671a609971eaeae
Daniel Reich <danreich@zedat.fu-berlin.de>
2018-09-14 15:09:36 +0200

    Added Riemann and GLL two-particle integrals in rad_check
    Also added routine to compute the DVR primitives at arbitrary grid points

--- radial_check.f90
+++ radial_check.f90
@@ -11,27 +11,24 @@ program radial_check
   real(idp), allocatable     :: matrix(:,:), matrix_full(:,:)
   real(idp), allocatable     :: matrix_all(:,:), matrix_inv_all(:,:), unity(:,:)
   real(idp), allocatable     :: Tkin_cardinal(:,:)
-  integer                    :: i, j, a, b, l, l_val
+  integer                    :: i, j, a, b, c, d, l, l_val
   real(idp),  allocatable    :: file_r(:), file_pot(:)
   integer, allocatable       :: ipiv(:)
-  real(idp)                  :: full_r_max
+  real(idp)                  :: curr_r, curr_r_prime, integral_val, dr
   real(idp), allocatable     :: work(:)
   integer                    :: info
 
   l_val=0
 
-  ! IMPORTANT: It is crucial that the parameters used here for the radial
-  !            matrix elements are the same that will be used when obtaining
-  !            the eigenfunctions of the radial Schroedinger equation!
-
-  para%pottype       = 'file' ! 'density_file', 'file', 'analytical'
+  para%pottype       = 'analytical' ! 'density_file', 'file', 'analytical'
   para%pot_filename  = 'input_pot.in' 
   para%r_min         = 0.0
-  para%r_max         = 300.0
+  para%r_max         = 10.0
   para%m             = 200
   para%nl            = 5
-  para%nr            = 1001 !nr = m * nl + 1
-  para%l             = l_val !Rotational quantum number
+  para%nr            = 1001  !nr = m * nl + 1
+  para%l             = l_val !Multipole order
+  para%Z             = 1
   para%mass          = 1.0
 
   para%mapped_grid   = .false.
@@ -41,11 +38,6 @@ program radial_check
   para%E_max         = 1d-5
 
   call init_grid_dim_GLL(grid, para)
-
-  ! Note that later on the first and final grid point are removed from the grid
-  ! Since we need for the matrix elements the value of r_max of the original,
-  ! full grid, it will be stored here
-  full_r_max = maxval(grid%r)
  
   if (para%pottype == 'analytical') then
     write(*,*) 'Using analytical potential'
@@ -53,14 +45,6 @@ program radial_check
     do i = 1,size(pot)
        pot(i) = analytical_potential(grid%r(i), para%mass)
     end do
-    !open(11, file="input_pot.ana", form="formatted", &
-    !&    action="write")
-    !write(11,*) '# File emulating a V = - 1/r hydrogen potential'
-    !do i = 1, 100000
-    !  write(11,*) 300.0d0 * (real(i,idp)/100000d0),                            &
-    !  &           - one / (300.0d0 * (real(i,idp)/100000d0))
-    !end do
-    !close(11)
   elseif ((para%pottype == 'file') .or. (para%pottype == 'density_file')) then
     write(*,*) 'Using potential from file '//trim(para%pot_filename)
     call init_grid_op_file_1d(file_pot, file_r, para)
@@ -73,14 +57,91 @@ program radial_check
   call redefine_ops_cardinal(pot)
   call redefine_GLL_grid_1d(grid)
   
-  do j = 1, 20 
-    open(11,file="explicit_dvr_primitive"//trim(int2str(j))//".dat",form="formatted",&
-    &    action="write", recl=100000)
+  do j = 1, 15
+    open(11,file="explicit_dvr_primitive"//trim(int2str(j))//".dat",           &
+    &    form="formatted", action="write", recl=100000)
+    open(12,file="full_explicit_dvr_primitive"//trim(int2str(j))//".dat",      &
+    &    form="formatted", action="write", recl=100000)
     do i = 1, size(grid%r)
-      write(11,'(3ES25.17)') grid%r(i), grid%weights(i),                       &
-      &        dvr_primitive_val(j, para, grid, grid%r(i))*sqrt(grid%weights(i))
+      write(11,'(4ES25.17)') grid%r(i), sqrt(grid%weights(i)), sqrt(grid%gllw(mod(i,grid%nl))), &
+      &        dvr_primitive_val_esteban(j, para, grid, grid%r(i))
+    end do
+    do i = 1, 10001
+      curr_r = (i-1) * 0.00001d0
+      write(12,'(2ES25.17)') curr_r,                                           &
+      &        dvr_primitive_val_esteban(j, para, grid, curr_r)!/sqrt(grid%gllw(mod(j,para%nl)))
     end do
     close(11)
+    close(12)
   end do
+
+  dr = 0.0001d0  
+  do a = 1, 10
+  do b = 1, 10
+    do c = 1, 10
+    do d = 1, 10
+      open(13,file="numerical_integrals_l"//trim(int2str(l_val))//".dat",      &
+      &    form="formatted", action="write", recl=100000)
+      integral_val = zero
+      do i = 1, 2001
+        curr_r = (i-1) * dr
+        if (abs(dvr_primitive_val_esteban(a, para, grid, curr_r)) < 1d-16) cycle
+        if (abs(dvr_primitive_val_esteban(c, para, grid, curr_r)) < 1d-16) cycle
+        do j = 1, 2001
+          curr_r_prime = (j-1) * dr
+          if (curr_r + curr_r_prime < 1d-16) cycle
+          integral_val = integral_val +                                        &
+          &              dvr_primitive_val_esteban(a, para, grid, curr_r) *            &
+          &              dvr_primitive_val_esteban(c, para, grid, curr_r) *            &
+          &              dvr_primitive_val_esteban(b, para, grid, curr_r_prime) *      &
+          &              dvr_primitive_val_esteban(d, para, grid, curr_r_prime) *      &
+          &              ( min(curr_r, curr_r_prime)**(l_val) /                &
+          &                max(curr_r, curr_r_prime)**(l_val+1) ) * dr * dr
+          !write(*,*) integral_val, dvr_primitive_val(a, para, grid, curr_r),   &
+          !& dvr_primitive_val(b, para, grid, curr_r_prime), min(curr_r, curr_r_prime),&
+          !& max(curr_r, curr_r_prime)
+        end do
+      end do
+      write(13,'(4I4, ES25.17)') a, b, c, d, integral_val
+      write(*,*) 'Writing integral', a, b, c, d
+    end do
+    end do
+  end do
+  end do
+  close(13)
+  
+  do a = 1, 10
+  do b = 1, 10
+    do c = 1, 10
+    do d = 1, 10 
+      open(14,file="GLL_numerical_integrals_l"//trim(int2str(l_val))//".dat",  &
+      &    form="formatted", action="write", recl=100000)
+      integral_val = zero
+      do i = 1, para%nr 
+        curr_r = grid%r(i)
+        do j = 1, para%nr 
+          curr_r_prime = grid%r(j) 
+          if (curr_r + curr_r_prime < 1d-16) cycle
+          integral_val = integral_val +                                        &
+          &              dvr_primitive_val_esteban(a, para, grid, curr_r) *            &
+          &              dvr_primitive_val_esteban(c, para, grid, curr_r) *            &
+          &              dvr_primitive_val_esteban(b, para, grid, curr_r_prime) *      &
+          &              dvr_primitive_val_esteban(d, para, grid, curr_r_prime) *      &
+          &              ( min(curr_r, curr_r_prime)**(l_val) /                &
+          &                max(curr_r, curr_r_prime)**(l_val+1) ) *            &
+          &              grid%weights(i) * grid%weights(j)
+          !write(*,*) integral_val, dvr_primitive_val(a, para, grid, curr_r),   &
+          !& dvr_primitive_val(b, para, grid, curr_r_prime), min(curr_r, curr_r_prime),&
+          !& max(curr_r, curr_r_prime)
+        end do
+      end do
+      if (abs(integral_val) < 1d-16) integral_val = zero !truncate num. zeroes
+      write(14,'(4I4, ES25.17)') a, b, c, d, integral_val
+      write(*,*) 'Writing GLL integral', a, b, c, d
+    end do
+    end do
+  end do
+  end do
+  close(14)
 
 end program radial_check
--- radial_mod.f90
+++ radial_mod.f90
@@ -115,7 +115,76 @@ contains
     end if
 
   end subroutine wrap_dsytri
+  
+  !! @description: Legendre function
+  !! @param: j  Positive integer
+  !! @param: x  Argument with |x|<=1
+  real(idp) function legendre(j,x)
 
+    integer   :: j
+    real(idp) :: x
+
+    real(idp) :: pmmp1, pmm, fact, somx2, pll
+    integer   :: m, ji, ll, kk
+
+    m = 0
+
+    ji = j
+    if (m < 0 .or. m > ji .or. abs(x) > one) then
+      write(*,*) 'Bad arguments'
+    end if
+    pmm = one
+    if (m > 0) then
+      somx2 = sqrt((one - x) * (one + x))
+      fact = one
+      do kk = 1,m
+        pmm = -pmm * fact * somx2
+        fact = fact + two
+      end do
+    end if
+    if (ji == m) then
+      legendre = pmm
+    else
+      pmmp1 = x * (two * real(m,idp) + one) * pmm
+      if (ji == m + 1) then
+        legendre = pmmp1
+      else
+        do ll = m+2,ji
+          pll = (x * (two * real(ll,idp) - one) * pmmp1 - &
+          &     real(ll + m - 1,idp) * pmm) / real(ll - m,idp)
+          pmm = pmmp1
+          pmmp1 = pll
+        end do
+        legendre = pll
+      end if
+    end if
+
+  end function legendre
+  
+  !! @description: Derivative of Legendre function
+  !! @param: j  Positive integer
+  !! @param: x  Argument with |x|<=1
+  real(idp) function legendre_deriv(j, x)
+
+    integer   :: j
+    real(idp) :: x
+
+    if (j == 0) then
+      legendre_deriv = 0
+      return
+    end if
+
+    legendre_deriv = (real(j, kind=idp) / (x**2 - 1)) *                        &
+    &                (x * legendre(j,x) - legendre(j-1,x))
+
+
+  end function legendre_deriv
+
+  !! @description: Calculates DVR eigenfunction for arbitrary point on real axis 
+  !! @param: ind   Index of DVR eigenfunction
+  !! @param: para  Parameter data
+  !! @param: grid  Spatial grid
+  !! @param: r     Evaluation point
   real(idp) function dvr_primitive_val(ind, para, grid, r)
 
     integer,                    intent(in) :: ind
@@ -125,64 +194,139 @@ contains
 
     integer :: m_val
     integer :: i_val
+    integer :: i
+    real(idp) :: r_min, r_max
+    real(idp) :: xi, xi_ref, xi_curr
 
     i_val = ind / para%nl
     m_val = mod(ind, para%nl)
-
-    !write(*,*) ind, i_val, m_val
-
-    if (m_val == 0) then
-      !Bridge function
-      !write(*,*) 'BRIDGE'
-      dvr_primitive_val = (dvr_product(r, i_val - 1, para%nl - 1, para, grid) +&
-      &                    dvr_product(r, i_val, 0, para, grid)) /         &
-      &                    sqrt(grid%weights(ind) + grid%weights(ind+1))
+    if (i_val == 0) then
+      r_min = zero
     else
-      !write(*,*) 'STUFF'
-      dvr_primitive_val = (dvr_product(r, i_val, m_val, para, grid) /          &
-      &                   sqrt(grid%weights(ind)))
+      r_min = grid%r(i_val*para%nl)
     end if
-
-  end function dvr_primitive_val
-  
-  real(idp) function dvr_product(r, i_val, m_val, para, grid)
-
-    real(idp),                  intent(in) :: r
-    integer,                    intent(in) :: i_val
-    integer,                    intent(in) :: m_val
-    type(para_t),               intent(in) :: para 
-    type(grid_t),               intent(in) :: grid 
-
-    integer :: k, ind
-    real(idp) :: r_min, r_max
-
-    ind = i_val * para%nl + m_val
-    r_min = grid%r(i_val*para%nl)
-    r_max = grid%r(i_val*para%nl+para%nl-1)
-
+    r_max = grid%r(i_val*para%nl+para%nl)
+    
     if (r < r_min) then
-      dvr_product = zero
+      dvr_primitive_val = zero
       return 
     end if
     if (r > r_max) then
-      dvr_product = zero
+      dvr_primitive_val = zero
       return
     end if
+
+    xi     = two * ((r           - r_min) / (r_max - r_min)) - one!map to [-1,1]
+    xi_ref = two * ((grid%r(ind) - r_min) / (r_max - r_min)) - one!map to [-1,1]
+
+    if (abs(xi) > one) then
+      dvr_primitive_val = zero
+      return 
+    end if
+
+    dvr_primitive_val = - ( legendre_deriv(para%nl,xi) * (one - xi**2)) /      & 
+    &             (real(para%nl*(para%nl+1),kind=8)                            &
+    &             *legendre(para%nl,xi_ref)*(xi-xi_ref))
+
+  end function dvr_primitive_val
+  
+  !! @description: Calculates DVR eigenfunction for arbitrary point on real axis 
+  !! @param: ind   Index of DVR eigenfunction
+  !! @param: para  Parameter data
+  !! @param: grid  Spatial grid
+  !! @param: r     Evaluation point
+  real(idp) function dvr_primitive_val_esteban(ind, para, grid, r)
+
+    integer,                    intent(in) :: ind
+    type(para_t),               intent(in) :: para
+    type(grid_t),               intent(in) :: grid 
+    real(idp),                  intent(in) :: r
+
+    integer :: m_val
+    integer :: i_val
+    integer :: i
+    real(idp) :: r_min, r_max
+    real(idp) :: xi, xi_ref, xi_curr
+    real(idp) :: numerator, denominator
     
-    !write(*,*) i_val*para%nl, i_val*para%nl+para%nl
-    !write(*,*) r, r_min, r_max
+    i_val = ind / para%nl
+    m_val = mod(ind, para%nl)
+    if (i_val == 0) then
+      r_min = zero
+    else
+      r_min = grid%r(i_val*para%nl)
+    end if
+    r_max = grid%r(i_val*para%nl+para%nl)
+    
+    if (r < r_min) then
+      dvr_primitive_val_esteban = zero
+      return 
+    end if
+    if (r > r_max) then
+      dvr_primitive_val_esteban = zero
+      return
+    end if
 
-    dvr_product = one
-    write(*,*) ind, i_val, m_val 
-    do k = 0, para%nl - 1
-      if (k == m_val) cycle
-      write(*,*) r, k, grid%r(i_val*para%nl+k) 
-      !write(*,*) r-grid%r(i_val*para%nl+k), grid%r(ind), grid%r(i_val*para%nl+k)
-      !write(*,*) i_val*para%nl+k, ind, i_val, m_val
-      dvr_product = dvr_product * ((r - grid%r(i_val*para%nl+k)) /             &
-      &                            (grid%r(ind) - grid%r(i_val*para%nl+k)))
-    end do
+    xi     = two * ((r           - r_min) / (r_max - r_min)) - one!map to [-1,1]
+    xi_ref = two * ((grid%r(ind) - r_min) / (r_max - r_min)) - one!map to [-1,1]
 
-  end function dvr_product
+    if (abs(xi) > one) then
+      dvr_primitive_val_esteban = zero
+      return 
+    end if
+
+    if (abs(xi - xi_ref) < 1d-16) then
+      dvr_primitive_val_esteban = one
+      return
+    end if
+
+    numerator = DerivLegPol(xi,para%nl) * (one - xi**2)
+    denominator = real(para%nl*(para%nl+1),kind=8)                             &
+    &             *LegPol(xi_ref,para%nl)*(xi-xi_ref)
+    dvr_primitive_val_esteban = - numerator / (denominator ) 
+
+  end function dvr_primitive_val_esteban
+   
+  real(idp) function LegPol(point, nl)
+
+    implicit none
+    real(idp), intent(in)    :: point
+    integer,   intent(in)    :: nl 
+   
+    real(idp) :: Leg(0:nl)
+    real(idp) :: fac0, fac1 
+    integer   :: j 
+
+    Leg(0) = one 
+    Leg(1) = point 
+    do j = 1, nl-1 
+      fac1 = (two*real(j,kind = 8) + one) / (real(j,kind=8) + one) 
+      fac0 = real(j,kind = 8)  / (real(j,kind=8) + one) 
+      Leg(j+1) = point *  Leg(j) * fac1 - Leg(j-1) * fac0  
+    enddo
+    LegPol = Leg(nl)
+
+  end function LegPol 
+   
+  real(idp) function DerivLegPol(point, nl)
+
+    implicit none
+    real(idp), intent(in)    :: point
+    integer,   intent(in)    :: nl 
+  
+    real(idp) :: DLeg(0:nl)
+    real(idp) :: fac0, fac1 
+    integer   :: j 
+   
+    DLeg(0) = zero 
+    DLeg(1) = one 
+    do j = 1, nl-1 
+      fac1 = (real(2*j,kind = 8) + one) / real(j,kind=8) 
+      fac0 = real(j+1,kind = 8)  / (real(j,kind=8) ) 
+      DLeg(j+1) = point *  DLeg(j) * fac1 - DLeg(j-1) * fac0  
+    enddo
+    DerivLegPol = DLeg(nl)
+
+  end function DerivLegPol 
 
 end module radial_mod
