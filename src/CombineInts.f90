0633201f1ffc00d430911cbd0db7524a7f9e182f
Pradipta Samanta <samanta@fkf.mpg.de>
2018-09-24 17:56:37 +0200

    fixed another bug for calculating 2e integrals by including the effect of the quantum number l
    Former-commit-id: 245bfc36c197cd48723ed5b9110e08ebcd56aa6f
    Former-commit-id: 96fb5f6dfb5b2c1f7cf8aeb37863469e9207c50f
    Former-commit-id: 1ec34d6a6eb543b5b073faf1033232ce9d39aa6a

--- src/CombineInts.f90
+++ src/CombineInts.f90
@@ -257,62 +257,62 @@ module CombineInts
       n_m1 = 2*l1 - 1
       m1_init = -1*l1
 
-      do l3 = 1, n_l
-        lc = l3 - 1
-        n_m3 = 2*l3 - 1
-        m3_init = -1*l3
+!     do l3 = 1, n_l
+!       lc = l3 - 1
+!       n_m3 = 2*l3 - 1
+!       m3_init = -1*l3
 
         do l2 = 1, n_l
           lb = l2 - 1
           n_m2 = 2*l2 - 1
           m2_init = -1*l2
 
-          do l4 = 1, n_l
-            ld = l4 - 1
-            n_m4 = 2*l4 - 1
-            m4_init = -1*l4
+!         do l4 = 1, n_l
+!           ld = l4 - 1
+!           n_m4 = 2*l4 - 1
+!           m4_init = -1*l4
 
 
             do m1 = 1, n_m1
               ma = m1_init + m1
               lma = (l1-1)**2 + m1
               !lma = (l1-1)**2 + int(la) + int(ma) + 1
-              do m3 = 1, n_m3
-                mc = m3_init + m3
-                lmc = (l3-1)**2 + m3
+              do m3 = 1, n_m1
+                mc = m1_init + m3
+                lmc = (l1-1)**2 + m3
                 !lmc = (l3-1)**2 + int(lc) + int(mc) + 1
 
                 do m2 = 1, n_m2
                   mb = m2_init + m2
                   lmb = (l2-1)**2 + m2
                   !lmb = (l2-1)**2 + int(lb) + int(mb) + 1
-                  do m4 = 1, n_m4
-                    md = m4_init + m4
-                    lmd = (l4-1)**2 + m4
+                  do m4 = 1, n_m2
+                    md = m2_init + m4
+                    lmd = (l2-1)**2 + m4
                     !lmd = (l4-1)**2 + int(ld) + int(md) + 1
 
                     do k1 = 1, orb%n_max - l1 + 1
                       do k2 = 1, orb%n_max -l2 + 1  
-                        do k3 = 1, orb%n_max - l3 + 1
-                          do k4 = 1, orb%n_max -l4 + 1  
+                        do k3 = 1, orb%n_max - l1 + 1
+                          do k4 = 1, orb%n_max -l2 + 1  
                             indx = indx + 1
                             int_value = 0.0d0
-            
+
                             klm_1 = SpatialOrbInd(k1,l1,m1)
                             klm_2 = SpatialOrbInd(k2,l2,m2)
-                            klm_3 = SpatialOrbInd(k3,l3,m3)
-                            klm_4 = SpatialOrbInd(k4,l4,m4)
-            
+                            klm_3 = SpatialOrbInd(k3,l1,m3)
+                            klm_4 = SpatialOrbInd(k4,l2,m4)
+
                             do l = 1, 2*para%l + 1
                               int_value = int_value + (integrals_ang(l, lma, lmb, lmc, lmd)* &
-                              &  TwoERadOrbInts(k1,k2,k3,k4,l))
-                              if (klm_1.eq.1.and.klm_2.eq.1.and.klm_3.eq.1) then
-!                               write(77,'(7I4,X,2F15.10)') k1, k2, lma, lmb, lmc, lmd, klm_4, integrals_ang(l, lma, lmb, lmc, lmd), TwoERadOrbInts(k1,k2,l)
-                              end if
+                              &  TwoERadOrbInts(k1,k2,k3,k4,l1,l2,l))
+!                             if (klm_1.eq.1.and.klm_2.eq.1.and.klm_3.eq.1) then
+!                               write(77,'(7I4,X,2F15.10)') k1, k2, lma, lmb, lmc, lmd, klm_4, integrals_ang(l, lma, lmb, lmc, lmd), TwoERadOrbInts(k1,k2,k3,k4,l1,l2,l)
+!                             end if
                             end do
-                              if (klm_1.eq.1.and.klm_2.eq.1.and.klm_3.eq.1) then
+!                             if (klm_1.eq.1.and.klm_2.eq.1.and.klm_3.eq.1) then
 !                               write(77,*) ''
-                              end if
+!                             end if
                             TwoEInts(klm_1, klm_2, klm_3, klm_4) = int_value
 !                           write(77, '(6I5,X,f15.10)') k1, k2, lma, lmb, lmc, lmd, int_value
 !                           write(78, '(4I5,X,f15.10)') klm_1, klm_2, klm_3, klm_4, int_value
@@ -327,9 +327,9 @@ module CombineInts
               end do
             end do
 
-          end do
+!         end do
         end do
-      end do  
+!     end do  
     end do  
 
 !   write(iout, *) 'Index:', indx
