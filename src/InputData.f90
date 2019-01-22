2b0fbd604d9b04099c12df8a393a3c10d408e273
Pradipta Samanta <samanta@fkf.mpg.de>
2019-01-22 13:42:38 +0100

    changed the input structure for the option split_grid
    Former-commit-id: c362524905a84e53936e92502fdb76ed1ac2692d

--- src/InputData.f90
+++ src/InputData.f90
@@ -7,9 +7,9 @@ module InputData
 
   save 
 
-  real(dp) :: r_min, r_max, r_max1, r_max2, mass, beta, e_max, nev_fac
-  integer  :: m, m1, m2, nl, nr, l_max, z, n_max, two_e_int, nfrz
-  logical  :: mapped_grid, only_bound, dvr_diag, dvr_integrals, trans_integrals, orbital_ints
+  real(dp) :: r_min, r_max, r_interm, r_max1, r_max2, mass, beta, e_max, nev_fac
+  integer  :: m(2), m1, m2, nl, nr, l_max, z, n_max, two_e_int, nfrz
+  logical  :: mapped_grid, only_bound, dvr_diag, dvr_integrals, trans_integrals, orbital_ints, split_grid
   character(255) :: maptype, read_envelop, pottype, pot_filename, read_envelope, diagtype
 
 
