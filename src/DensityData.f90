c877e6d4f14209f0262adcba5117cdc64e95c9a3
Pradipta Samanta <samanta@fkf.mpg.de>
2019-03-12 16:06:20 +0100

    transformation of RDMs in the primitive basis is done
    Former-commit-id: 83953770f328048a2fc9c2f50755e7d4170a6982
    Former-commit-id: cb56a0da0a024bac7c2b3b7426ad84a1a98e6d51
    Former-commit-id: 2155cef7acb8de5deddb44375872cefbc3ebf12f

--- src/DensityData.f90
+++ src/DensityData.f90
@@ -6,9 +6,10 @@ module DensityData
 
     save
 
-    real(dp) :: r_val
+    real(dp) :: r_val, Yield1e, Yield2e
     integer  :: n_orb_den(2), tot_orb
     character(len=32) :: file_1rdm, file_2rdm
     complex(idp), allocatable :: DensOrb1e(:), DensOrb2e(:,:)
+    complex(idp), allocatable :: PrimDens1e(:), PrimDens2e(:)
 
 end module DensityData
