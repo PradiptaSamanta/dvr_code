8aa20dea73a3e46e3acfc4b0a81eeb4de4efec96
Pradipta Samanta <samanta@fkf.mpg.de>
2019-03-13 19:28:25 +0100

    Calculation of photoionization yield outside a spherical region is implemented
    Former-commit-id: 1aa88b49933a4a31a53ece9f22bb7c0568c7128f [formerly fa2fb21571dff236164eca81fc2b60a722feb81d]
    Former-commit-id: accadb57662ab67adcc0cd1c2d406ab8199e3e6d [formerly a60338dbd82e7d6879a6af2366e6b53ee8f24d5b]
    Former-commit-id: 04860fff3dce82a63c82c2c18e3f7ec0396a3526 [formerly 7fba045a778ac928c2f31a85f78e31028f6127ee]
    Former-commit-id: 6cf0100708d6e154bd32bf9beb23a223eb66ffcc

--- src/Density.f90
+++ src/Density.f90
@@ -10,53 +10,113 @@ module Density
   subroutine CalcDensity(EigVecs)
 
     use DensityData
-    use DVRData, only : para
+    use DVRData, only : para, grid
   
     real(dp), allocatable, intent(in) :: EigVecs(:,:,:)
 
     real(dp), allocatable :: MOCoeff(:,:)
-    integer, allocatable :: OrbInd(:,:,:)
-    integer :: n_l, n_nqn, i, j, k, error, tot_prim
+    integer, allocatable :: OrbInd_cntr(:,:,:), OrbInd_prim(:,:,:)
+    integer :: n_l, n_cntr, i, j, k, error, tot_prim, n_prim, start_prim, tot_cntr
     real(dp) :: start, finish
 
     call cpu_time(start)
 
+    ! upper limit for the number of l quantum number
     n_l = para%l + 1
-    n_nqn = sum(n_orb_den)
-    if (n_nqn.lt.0) call stop_all('CalcDenstiy','Number of orbitals are put wrong in the input')
+    ! upper limit for the n quantum number in the contracted basis in which the RDMs will be read
+    n_cntr = sum(n_orb_den)
+    if (n_cntr.lt.0) call stop_all('CalcDenstiy','Number of orbitals are put wrong in the input')
 
-    allocate(OrbInd(para%ng, n_l,2*n_l-1), stat=error)
+    ! First, the number of n quantum number which will be involved in calculating the photonization yield (n_prim) is calculated.
+    ! Photonization yied is calculated by integrating electron density outside a certian spherical region. That is why
+    ! the initial grid points are not needed while calculating the yield
+    if (abs(r_val).gt.1e-8) then
+      call CalcStartOrb(grid%r, para%ng, r_val, n_prim, start_prim)
+    else 
+      start_prim = 1
+      n_prim = para%ng
+    end if
+
+    ! The array that gives the orbital index in the primitive basis is allocated here
+    allocate(OrbInd_cntr(n_cntr, n_l,2*n_l-1), stat=error)
     call allocerror(error)
+    OrbInd_cntr = 0
+    allocate(OrbInd_prim(n_prim, n_l,2*n_l-1), stat=error)
+    call allocerror(error)
+    OrbInd_prim = 0
 
-    tot_prim = 0
-    do i = 1, para%ng
+    ! Get the total number of orbitals involved in the contracted basis (tot_cntr) and 
+    ! an array that gives the index of a contraced orbital for given, n, l and m
+    tot_cntr = 0
+    do i = 1, n_cntr
       do j = 1, min(n_l,i)
         do k = 1, 2*j-1
-          tot_prim = tot_prim + 1
-          OrbInd(i,j,k) = tot_prim
-          if (i.eq.n_nqn) tot_orb = tot_prim
+          tot_cntr = tot_cntr + 1
+          OrbInd_cntr(i,j,k) = tot_cntr
         end do
       end do
     end do
 
-    ! First read one and two electron RDMs obtained from a real-time simulation
-    call ReadOneRDM(DensOrb1e, file_1rdm, tot_orb)
-    call ReadTwoRDM(DensOrb2e, file_2rdm, tot_orb)
+    ! Get the total number of orbitals involved in the primitive basis (tot_prim) and
+    ! an array that gives the index of a primitive orbital for given, n, l and m
+    tot_prim = 0
+    do i = 1, n_prim
+      !do j = 1, min(n_l,i)
+      do j = 1, n_l
+        do k = 1, 2*j-1
+          tot_prim = tot_prim + 1
+          OrbInd_prim(i,j,k) = tot_prim
+        end do
+      end do
+    end do
+
+    ! First read one and two electron RDMs obtained from a real-time (FCIQMC) simulation
+    call ReadOneRDM(DensOrb1e, file_1rdm, tot_cntr)
+    call ReadTwoRDM(DensOrb2e, file_2rdm, tot_cntr)
 
     ! Transform one and two electron RDMs from the MO orbitals basis to the basis of the primitive orbitals
-    call GetOrbCoeff(EigVecs, MOCoeff, tot_orb, tot_prim, para%ng, n_nqn, n_l, OrbInd)
-    call TransformDens1e(DensOrb1e, PrimDens1e, MOCoeff, tot_orb, tot_prim)
-    call TransformDens2e(DensOrb2e, PrimDens2e, MOCoeff, tot_orb, tot_prim)
+    call GetOrbCoeff(EigVecs, MOCoeff, tot_cntr, tot_prim, start_prim, n_prim, n_cntr, n_l, OrbInd_cntr, OrbInd_prim)
+    call TransformDens1e(DensOrb1e, PrimDens1e, MOCoeff, tot_cntr, tot_prim)
+    call TransformDens2e(DensOrb2e, PrimDens2e, MOCoeff, tot_cntr, tot_prim)
 
     call Get1eYield(PrimDens1e, Yield1e, tot_prim)
     call Get2eYield(PrimDens2e, Yield2e, tot_prim)
 
+    deallocate(OrbInd_cntr, OrbInd_prim)
     deallocate(DensOrb1e, DensOrb2e)
 
     call cpu_time(finish)
     write(iout,'(a, f10.5)') ' Time taken to calculate the Density: ', finish - start
 
   end subroutine CalcDensity
+
+  ! The following subroutine cut the total array of grid points at 'r_val'
+  ! It returns the number of grid points after 'r_val' (n_prim) and 
+  ! the position of this particular grid point in the whole array (start_prim)
+  subroutine CalcStartOrb(grid_r, ng, r_val, n_prim, start_prim)
+
+    real(dp), allocatable, intent(in) :: grid_r(:)
+    integer, intent(in) :: ng
+    real(dp), intent(in) :: r_val
+    integer, intent(out) :: n_prim, start_prim
+
+    real(dp) :: r_diff, sign_diff
+    integer :: i
+    if (size(grid_r).ne.ng) call stop_all('CalcStartOrb', 'Somehow number of grid points does not match')
+    
+    do i = 1, ng
+      r_diff = r_val - grid_r(i)
+      sign_diff = sign(1.0d0, r_diff)
+      if (sign_diff.eq.-1.0d0) then
+        n_prim = ng - i + 1
+        start_prim = i
+        exit
+      else 
+        cycle
+      end if
+    end do
+
+  end subroutine CalcStartOrb
 
   subroutine ReadOneRDM(Dens1e, file_1, tot_orb)
 
@@ -121,7 +181,7 @@ module Density
       if (i.le.j) then
         ij = i*(i-1)/2 + j 
         Dens1e(ij) = cmplx(real(Dens1e(ij)), val)
-        write(81, '(2i5, f25.17)') i, j, val
+        !write(81, '(2i5, f25.17)') i, j, val
       else
         cycle
       endif
@@ -222,14 +282,14 @@ module Density
 
   end subroutine ReadTwoRDM
 
-  subroutine GetOrbCoeff(EigVecs, MOCoeff, tot_orb, tot_prim, ng, n_nqn, n_l, OrbInd)
+  subroutine GetOrbCoeff(EigVecs, MOCoeff, tot_orb, tot_prim, ni, ng, n_cntr, n_l, OrbInd_cntr, OrbInd_prim)
 
     real(dp), allocatable, intent(in) :: EigVecs(:,:,:)
     real(dp), allocatable, intent(out) :: MOCoeff(:,:)
-    integer, allocatable, intent(in) :: OrbInd(:,:,:)
-    integer, intent(in) :: tot_orb, tot_prim, ng, n_nqn, n_l
+    integer, allocatable, intent(in) :: OrbInd_cntr(:,:,:), OrbInd_prim(:,:,:)
+    integer, intent(in) :: tot_orb, tot_prim, ni, ng, n_cntr, n_l
 
-    integer :: error, indx_1, indx_2, n, i, l_val, l, m
+    integer :: error, indx_1, indx_2, n, i, l_val, l, m, i_p
     real(dp) :: val
 
     allocate(MOCoeff(tot_orb,tot_prim), stat=error)
@@ -237,14 +297,14 @@ module Density
 
     indx_1 = 0
     indx_2 = 0
-    do n = 1, n_nqn
-      do i = 1, ng
-        l_val =  min(i,n,n_l)
-        do l = 1, l_val
-          val = EigVecs(i,n,l)
+    do l = 1, n_l
+      do n = l, n_cntr
+        do i = 1, ng
+        i_p = i + ni - 1
+          val = EigVecs(i_p,n,l)
           do m = 1, 2*l-1
-            indx_1 = OrbInd(n,l,m)
-            indx_2 = OrbInd(i,l,m)
+            indx_1 = OrbInd_cntr(n,l,m)
+            indx_2 = OrbInd_prim(i,l,m)
             MOCoeff(indx_1, indx_2) = val
             !write(77,'(6i4,f15.8)') n, l, m, i, indx_1, indx_2, MOCoeff(indx_1,indx_2)
           end do
