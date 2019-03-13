1aa88b49933a4a31a53ece9f22bb7c0568c7128f
Pradipta Samanta <samanta@fkf.mpg.de>
2019-03-13 19:28:25 +0100

    Calculation of photoionization yield outside a spherical region is implemented
    Former-commit-id: fa2fb21571dff236164eca81fc2b60a722feb81d

--- src/DensityData.f90
+++ src/DensityData.f90
@@ -7,7 +7,7 @@ module DensityData
     save
 
     real(dp) :: r_val, Yield1e, Yield2e
-    integer  :: n_orb_den(2), tot_orb
+    integer  :: n_orb_den(2)
     character(len=32) :: file_1rdm, file_2rdm
     complex(idp), allocatable :: DensOrb1e(:), DensOrb2e(:,:)
     complex(idp), allocatable :: PrimDens1e(:), PrimDens2e(:)
